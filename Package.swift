c8e780bcb8bcd39b63c4392c095a1af5d30da49f
minozzicaf <vinicius.minozzi@caf.io>
2025-04-01 12:23:53 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/combateafraude/CafSolutions.git",
-            .exact("1.0.2")
+            .exact("2.0.3")
         )
     ],
     targets: [
